38f991e568778bf6de2e2de52ab44b59d29d9ab6
seedotlee <lisi_1987@foxmail.com>
2025-06-18 16:11:33 +0800

    Update queue_download.sh

--- queue_download.sh
+++ queue_download.sh
@@ -7,15 +7,13 @@ https://p19-oec-ttp.tiktokcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d640639554
 https://p16-oec-ttp-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg \
 https://p19-oec-ttp-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg"
 
-# 兼容 dash 的循环方式
 i=1
 while [ "$i" -le 20 ]; do
   echo "============= 第 $i 轮下载 ============="
-  
+
   for url in $URLS; do
     echo "下载中: $url"
 
-    # 下载文件，设置 30 秒超时，保存 header
     curl -m 30 -s -o ./tmp_output -D ./tmp_header -L "$url" \
       -w "平均速度: %{speed_download} bytes/s\n下载时间: %{time_total} 秒\n" > ./curl_output.log
 
@@ -24,8 +22,7 @@ while [ "$i" -le 20 ]; do
     echo "URL: $url"
 
     if [ "$code" = "0" ]; then
-      # 提取状态码
-      status=$(grep -i "^HTTP/" ./tmp_header | tail -1 | cut -d' ' -f2)
+      status=$(grep -i "^HTTP/" ./tmp_header | tail -1 | awk '{print $2}')
       echo "状态码: $status"
       cat ./curl_output.log
 
@@ -46,5 +43,5 @@ while [ "$i" -le 20 ]; do
     echo "----------------------------------------"
   done
 
-  i=$((i + 1))  # dash 支持这种形式的算术运算
+  i=$((i + 1))
 done

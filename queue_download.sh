63916dd23c7a08e6c90aefa5afea3a2a860e56ba
seedotlee <lisi_1987@foxmail.com>
2025-06-19 10:28:35 +0800

    Update queue_download.sh

--- queue_download.sh
+++ queue_download.sh
@@ -12,10 +12,14 @@ echo "--- 脚本将执行 $count 轮数据下载 ---"
 
 # 用空格分隔的 URL 字符串
 URLS="\
-https://p16-oec-ttp.tiktokcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg \
-https://p19-oec-ttp.tiktokcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg \
-https://p16-oec-ttp-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg \
-https://p19-oec-ttp-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg"
+https://p16-oec-va.ibyteimg.com/tos-maliva-i-o3syd03w52-us/94b919c403c8eec72bafcfeef82cb9f4.JPG~tplv-o3syd03w52-origin-jpeg.jpeg \
+https://p19-oec-va.ibyteimg.com/tos-maliva-i-o3syd03w52-us/94b919c403c8eec72bafcfeef82cb9f4.JPG~tplv-o3syd03w52-origin-jpeg.jpeg" 
+
+# https://p16-oec-ttp-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg \
+# https://p16-oec-ttp.tiktokcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg \
+# https://p19-oec-ttp.tiktokcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg \
+# https://p16-oec-ttp-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg \
+# https://p19-oec-ttp-useast5.ttcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/b1c62d6406395542c4d5dc9601347199.JPG~tplv-omjb5zjo8w-origin-jpeg.jpeg"
 
 i=1
 while [ "$i" -le "$count" ]; do
